0ddad127259a8b162becc55759eb9f2284f2eaac
seohyundeveloper <seoandjisister77@gmail.com>
2022-12-09 01:06:00 +0900

    레시피 만들기기

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes = [];
+  recipes:Recipe[] = [
+    new Recipe( 'A Test Recipe',
+                'This is simply a test.', 
+                'https://assets.epicurious.com/photos/555b5db3644d45515b757513/1:1/w_2240,c_limit/56389541_pasta-alla-gricia_1x1.jpg'),
+    new Recipe('My Favorite Recipe',
+               'This is one of the popular food.',
+               'https://assets.epicurious.com/photos/61f8150ca2bbc00670179f64/1:1/w_640,c_limit/CacciatoreBakedPasta_RECIPE_IG_012322_26052_VOG_final.jpg')
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+ 
