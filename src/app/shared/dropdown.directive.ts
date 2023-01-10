546a45ea67aed4f65d0ff7fe2d505f4412ee797c
seohyundeveloper <seoandjisister77@gmail.com>
2023-01-11 00:50:37 +0900

    add

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, HostBinding, HostListener } from "@angular/core";
+import { Directive, ElementRef, HostBinding, HostListener } from "@angular/core";
 
 @Directive({
   selector:'[appDropdown]'
@@ -7,7 +7,11 @@ export class DropdownDirective {
   // 클릭할때마다 첨부하거나 두번쩨 클릭시 제거하는 디렉티브 만들기
   
   @HostBinding('class.open') isOpen = false;
-  @HostListener('click') toggleOpen() {
-    this.isOpen = !this.isOpen;
+  @HostListener('document:click', ['$event']) toggleOpen(event:Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+  }  // document에 리스너를 배치함으로써 아무곳이나 클릭해 드롭다운이 닫히도록 설정하기
+
+  constructor( private elRef: ElementRef){
+
   }
 }
