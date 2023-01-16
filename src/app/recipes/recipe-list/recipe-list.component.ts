d428da00dbe82ae48fe14852b9a4088f54b011e0
seohyundeveloper <seoandjisister77@gmail.com>
2023-01-17 01:20:10 +0900

    레시피 서비스 채우기

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, EventEmitter, Output} from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -9,17 +10,20 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes:Recipe[] = [
-    new Recipe( 'A Test Recipe',
-                'This is simply a test.', 
-                'https://assets.epicurious.com/photos/555b5db3644d45515b757513/1:1/w_2240,c_limit/56389541_pasta-alla-gricia_1x1.jpg'),
-    new Recipe('My Favorite Recipe',
-               'This is one of the popular food.',
-               'https://assets.epicurious.com/photos/61f8150ca2bbc00670179f64/1:1/w_640,c_limit/CacciatoreBakedPasta_RECIPE_IG_012322_26052_VOG_final.jpg')
+    // new Recipe( 'A Test Recipe',
+    //             'This is simply a test.', 
+    //             'https://assets.epicurious.com/photos/555b5db3644d45515b757513/1:1/w_2240,c_limit/56389541_pasta-alla-gricia_1x1.jpg'),
+    // new Recipe('My Favorite Recipe',
+    //            'This is one of the popular food.',
+    //            'https://assets.epicurious.com/photos/61f8150ca2bbc00670179f64/1:1/w_640,c_limit/CacciatoreBakedPasta_RECIPE_IG_012322_26052_VOG_final.jpg')
   ];
 
-  constructor() { }
+  constructor(
+    private recipeService: RecipeService
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
   onRecipeSelected(recipe:Recipe) {
     this.recipeWasSelected.emit(recipe)
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,16 @@
+import { Recipe } from "./recipe.model";
+
+export class RecipeService {
+  private recipes:Recipe[] = [
+    new Recipe( 'A Test Recipe',
+                'This is simply a test.', 
+                'https://assets.epicurious.com/photos/555b5db3644d45515b757513/1:1/w_2240,c_limit/56389541_pasta-alla-gricia_1x1.jpg'),
+    new Recipe('My Favorite Recipe',
+               'This is one of the popular food.',
+               'https://assets.epicurious.com/photos/61f8150ca2bbc00670179f64/1:1/w_640,c_limit/CacciatoreBakedPasta_RECIPE_IG_012322_26052_VOG_final.jpg')
+  ];
+
+  getRecipes(){
+    return this.recipes.slice(); // 똑같은 복사본인 새배열을 반환함.
+  }
+}
