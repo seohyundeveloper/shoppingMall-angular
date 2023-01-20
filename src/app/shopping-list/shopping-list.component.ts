1aaaa7af2c74b0144c82ddcbfe74d6b28337b886
seohyundeveloper <seoandjisister77@gmail.com>
2023-01-21 01:33:21 +0900

    shoppingList  서비스 이용해서 재료 추가시키기

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,18 +8,20 @@ import { Ingredient } from '../shared/ingredient.model';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients:Ingredient[] = [
-    new Ingredient('Apple', 5),
-    new Ingredient('Tomato', 10)
-  ];
+  // ingredients:Ingredient[] = [
+  //   new Ingredient('Apple', 5),
+  //   new Ingredient('Tomato', 10)
+  // ];
+  ingredients:Ingredient[];
+  constructor(private slService: ShoppingListService) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.ingredients = this.slService.getIngredients(); 
+    //여기서 재료를 할당해준다.
   }
 
-  onIngredientAdded(ingredient: Ingredient) {
-    console.log(ingredient);
-    this.ingredients.push(ingredient);
-  }
+  // onIngredientAdded(ingredient: Ingredient) {
+  //   console.log(ingredient);
+  //   this.ingredients.push(ingredient);
+  // }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,17 @@
+import { Ingredient } from "../shared/ingredient.model";
+
+
 export class ShoppingListService {
-  
+  private  ingredients:Ingredient[] = [
+    new Ingredient('Apple', 5),
+    new Ingredient('Tomato', 10)
+  ];
+
+  getIngredients() {
+    return this.ingredients.slice(); // 복사본 
+  }
+
+  addIngredient(ingredient : Ingredient){
+      this.ingredients.push(ingredient); //재료를 추가해주기
+  }
 }
