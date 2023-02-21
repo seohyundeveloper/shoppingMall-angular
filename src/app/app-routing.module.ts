7e5d80cf0ee93b5c373bdb7aa6893a3573dfcd3a
seohyundeveloper <seoandjisister77@gmail.com>
2023-02-22 01:12:02 +0900

    edit컴포넌트 생성

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
 import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
+import { RecipesEditComponent } from "./recipes/recipes-edit/recipes-edit.component";
 import { RecipesComponent } from "./recipes/recipes.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 
@@ -9,7 +10,9 @@ const appRoutes: Routes = [
   { path:'' , redirectTo: '/recipes', pathMatch: 'full'},
   { path: 'recipes', component: RecipesComponent, children: [
     { path: '', component: RecipeStartComponent },
-    { path: ':id', component: RecipeDetailComponent}
+    { path: ':id', component: RecipeDetailComponent},
+    { path: 'new', component: RecipesEditComponent},
+    { path: ':id/edit', component: RecipesEditComponent}
   ] },
   { path: 'shopping-list', component: ShoppingListComponent }
 ]
